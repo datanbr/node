30a65038ab85bad44b0fd9336102140eb3802515
nicolasb <nicolas@ouidata.org>
2021-08-02 09:57:01 +0200

    rajout du port

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;  
 
 
 app.get('/', (req, res) => {
     res.send("Welcom to the home page");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('wassa');
 });
 
